053c39575e5b20295a3a664ca89ae44334f79413
inkbiegel <jellevuylsteke@gmail.com>
2014-06-22 23:04:43 +0200

    vanalles..

--- js/general.js
+++ js/general.js
@@ -1,6 +1,40 @@
 $(document).ready(function(){
-  siteNavIndicator();
+  // siteNavIndicator();
+  if($('.pageNav').length > 0){
+    pageNav();
+  }
 });
+
+function pageNav(){
+  var $pageNav = $('.pageNav'),
+      activeSection = $pageNav.find('a:first').attr('href').slice(1);
+
+  pageNavSetActiveStates();
+
+  $pageNav.find('a').on('click',function(e){
+    // e.preventDefault();
+
+    activeSection = $(this).attr('href').slice(1);
+    // $('.pageNav .active, .pageNavSection.active').removeClass('active');
+    $('.pageNav .active').removeClass('active');
+    $(this).addClass('active');
+
+    pageNavSetActiveStates();
+  });
+
+  function pageNavSetActiveStates(){
+    $pageNav.find($('a[href$="' + activeSection + '"]')).addClass('active');
+    // $('.pageNavSection#' + activeSection).addClass('active');
+  }
+}
+function setGmap(){
+  var mapOptions = {
+    center: new google.maps.LatLng(51.1348986,5.4256432),
+    zoom: 15
+  };
+  var map = new google.maps.Map(document.getElementById("gmap"),
+      mapOptions);
+}
 
 function siteNavIndicator(){
   var navInd = $.navIndicator({
