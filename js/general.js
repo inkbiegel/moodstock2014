9182d222b6b4de36071f4e3f00a305874e9065be
inkbiegel <jellevuylsteke@gmail.com>
2014-06-23 22:24:24 +0200

    info page

--- js/general.js
+++ js/general.js
@@ -3,37 +3,40 @@ $(document).ready(function(){
   if($('.pageNav').length > 0){
     pageNav();
   }
+  if($('#gmap').length > 0){
+    setGmap();
+  }
 });
 
 function pageNav(){
   var $pageNav = $('.pageNav'),
-      activeSection = $pageNav.find('a:first').attr('href').slice(1);
-
-  pageNavSetActiveStates();
+      pageNavLinks = [],
+      activeSectionNr = 1;
 
   $pageNav.find('a').on('click',function(e){
-    // e.preventDefault();
+    e.preventDefault();
 
-    activeSection = $(this).attr('href').slice(1);
-    // $('.pageNav .active, .pageNavSection.active').removeClass('active');
-    $('.pageNav .active').removeClass('active');
+    activeSectionNr = $(this).data('link');
+    $('.pageNav .active, .pageNavSection.active').removeClass('active');
     $(this).addClass('active');
-
-    pageNavSetActiveStates();
+    $('.pageNavSection:nth-of-type(' + activeSectionNr + ')').addClass('active');
   });
-
-  function pageNavSetActiveStates(){
-    $pageNav.find($('a[href$="' + activeSection + '"]')).addClass('active');
-    // $('.pageNavSection#' + activeSection).addClass('active');
-  }
 }
 function setGmap(){
+  var msLatlng = new google.maps.LatLng(51.134729, 5.426029);
   var mapOptions = {
-    center: new google.maps.LatLng(51.1348986,5.4256432),
+    center: new google.maps.LatLng(51.135167, 5.420879),
     zoom: 15
   };
-  var map = new google.maps.Map(document.getElementById("gmap"),
-      mapOptions);
+
+  var map = new google.maps.Map(document.getElementById("gmap"), mapOptions);
+
+  var marker = new google.maps.Marker({
+      position: msLatlng,
+      map: map,
+      title: 'Moodstock',
+      animation: google.maps.Animation.DROP
+  });
 }
 
 function siteNavIndicator(){
